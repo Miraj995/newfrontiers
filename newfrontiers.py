583fa34ef2723d20f00927b9147f0aed7412b518
Miraj995 <septicshocklifebringer@gmail.com>
2024-11-11 22:23:41 +0530

    Add files via upload
    master project

---
+++ newfrontiers.py
@@ -0,0 +1,51 @@
+n=str(input("enter any:"))
+m=str(input("enter any:"))
+e=len(str(n)+str(m))
+print(float(e))
+#f=str('E=mc2'+'E=hu')
+a=int('11111111',2)
+b=int('00000000',2)
+r=pow(a,b)
+c=pow(1,0)
+s=float(c*r**(b-a))
+o=float(s)
+soul=float(c*r**(a-a-a))
+mind=float(c*r**(b-b-b))
+word=float(c*r**(a-b-a))
+Nature=float(c*r**(b-a-b))
+Brain=float(c*r**(b-a-a))
+Gravity=float(c*r**(a-a-b))
+Time=float(c*r**(a-b-b))
+Space=float(c*r**(b-b-a))
+aum=float(soul)
+mmm=float(mind)
+Hridoy=float(word)
+prokriti=float(Nature)
+mogoj=float(Brain)
+contraction=float(Gravity)
+zero=float(Time)
+sky=float(Space)
+sky=str("boundless")
+if (True):
+    print(float(o))
+    if(True):
+        print(str(sky))
+
+class Time():
+    def pow (
+        base:2,
+        past=pow(1,0),
+        future=any
+        
+    ):
+        return(0)
+    print(Time)
+    
+    
+class words():
+    def str (
+            str=str("aaa"+"uuu"+"mmm"),
+                 
+     ):
+        return(pow(3))
+        
